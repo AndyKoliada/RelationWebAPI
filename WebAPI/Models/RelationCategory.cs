a3656fae49231bf3e9460c641e0505fc198a0377
Andrii Koliada <andykoliada@gmail.com>
2020-11-23 10:40:00 +0200

    Scaffolded DB

---
+++ WebAPI/Models/RelationCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models
+{
+    public partial class RelationCategory
+    {
+        public Guid RelationId { get; set; }
+        public Guid CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Relation Relation { get; set; }
+    }
+}
