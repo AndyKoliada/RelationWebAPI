a3ff3238e989b4dab8aae71d21e41a4502f57380
Andrii Koliada <andykoliada@gmail.com>
2020-11-23 16:07:52 +0200

    added modal window with buttons

--- WebAPI/ClientApp/src/app/relations/add-edit-relation/add-edit-relation.component.ts
+++ WebAPI/ClientApp/src/app/relations/add-edit-relation/add-edit-relation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-relation',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditRelationComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  @Input() relation: any;
+  RelationName: string;
 
   ngOnInit(): void {
+
+    this.RelationName = this.relation.RelationName;
   }
 
 }
--- WebAPI/ClientApp/src/app/relations/show-relation/show-relation.component.ts
+++ WebAPI/ClientApp/src/app/relations/show-relation/show-relation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-relation',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowRelationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  RelationsList: any = [];
+
+  ModalTitle: string;
+  ActivateAddEditRelationsComponent: boolean = false;
+  relation: any;
 
   ngOnInit(): void {
+    this.refreshRelationsList();
+  }
+
+  addClick() {
+    this.relation = {
+      RelationName:""
+    }
+    this.ModalTitle= "Add relation";
+    this.ActivateAddEditRelationsComponent= true;
+  }
+
+  closeClick() {
+    this.ActivateAddEditRelationsComponent = false;
+    this.refreshRelationsList();
+  }
+
+  editClick(item) {
+    this.relation = item;
+    this.ModalTitle = "Edit relation";
+    this.ActivateAddEditRelationsComponent = true;
+  }
+
+  refreshRelationsList() {
+    this.service.getRelationsList().subscribe(data => {
+      this.RelationsList = data;
+    });
   }
 
 }
