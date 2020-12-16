e83558258b9d747a8434567e41f6da62551f9536
Andrii Koliada <andykoliada@gmail.com>
2020-12-16 20:58:47 +0200

    Cleaned solution

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿INSERT INTO	[dbo].[tblRelation] (Id, Name, CreatedAt, CreatedBy, IsDisabled, IsTemporary, IsMe, PaymentViaAutomaticDebit, InvoiceDateGenerationOptions, InvoiceGroupByOptions )
-VALUES (NEWID(), 'John', GETDATE(), 'Admin', 'FALSE', 'FALSE', 'FALSE', 'FALSE', 1, 1);
-INSERT INTO	[dbo].[tblRelation] (Id, Name, CreatedAt, CreatedBy, IsDisabled, IsTemporary, IsMe, PaymentViaAutomaticDebit, InvoiceDateGenerationOptions, InvoiceGroupByOptions )
-VALUES (NEWID(), 'Peter', GETDATE(), 'Admin', 'FALSE', 'FALSE', 'FALSE', 'FALSE', 1, 1);
-INSERT INTO	[dbo].[tblRelation] (Id, Name, CreatedAt, CreatedBy, IsDisabled, IsTemporary, IsMe, PaymentViaAutomaticDebit, InvoiceDateGenerationOptions, InvoiceGroupByOptions )
-VALUES (NEWID(), 'James', GETDATE(), 'Admin', 'FALSE', 'FALSE', 'FALSE', 'FALSE', 1, 1);
-
