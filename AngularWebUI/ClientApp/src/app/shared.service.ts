c31c48f817327e36313c6ef55d12ab0f7d2eec06
Andrii Koliada <andykoliada@gmail.com>
2020-12-17 16:55:16 +0200

    Used native Angular environment.ts to inject api host url

--- AngularWebUI/ClientApp/src/app/shared.service.ts
+++ AngularWebUI/ClientApp/src/app/shared.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  readonly ApiUrl: string = "https://localhost:44358/api";
+  readonly ApiUrl: string = environment.apiHost;
   readonly ApiAlias: string = "/page";
   PageNumber: number = 1;
   PageSize: number = 5;
@@ -16,14 +16,13 @@ export class SharedService {
   FilterBy: string = "None";
   DeleteIdList: [];
   OrderByDescending: boolean = false;
-  
 
-  QueryString: string = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize 
-  + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy;
+  QueryString: string = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize
+    + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy;
 
   constructor(private http: HttpClient) { }
 
-  getRelationsList(): Observable<any[]>{
+  getRelationsList(): Observable<any[]> {
     console.log(this.QueryString);
     return this.http.get<any>(this.QueryString);
   }
@@ -36,25 +35,21 @@ export class SharedService {
     return this.http.put(this.ApiUrl + this.ApiAlias + "/" + id, val);
   }
 
-  sortRelationsList(sortBy: string) : Observable<any[]>{
+  sortRelationsList(sortBy: string): Observable<any[]> {
     this.OrderByDescending = !this.OrderByDescending;
     this.SortBy = sortBy;
-    this.QueryString = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize 
-    + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy; 
-
+    this.QueryString = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize
+      + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy;
     return this.http.get<any>(this.QueryString);
   }
 
-  changePage(pageNumber: number, pageSize: number) : Observable<any[]>{
+  changePage(pageNumber: number, pageSize: number): Observable<any[]> {
     this.PageNumber = pageNumber;
     this.PageSize = pageSize;
-    this.QueryString = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize 
-    + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy; 
-
+    this.QueryString = this.ApiUrl + this.ApiAlias + "/" + this.PageNumber + "/" + this.PageSize
+      + "/" + this.SortBy + "/" + this.OrderByDescending + "/" + this.FilterBy;
     return this.http.get<any>(this.QueryString);
   }
-
-  
 
   deleteRelation(id: string) {
     return this.http.delete(this.ApiUrl + this.ApiAlias + "/" + id);
